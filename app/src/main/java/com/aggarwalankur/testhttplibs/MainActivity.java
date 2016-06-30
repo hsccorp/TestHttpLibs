7f40501e9807962cfbcc3a1b0e8db90b66c5daaf
Ankur Aggarwal <roc.windwarrior@gmail.com>
2016-06-30 09:56:29 +0530

    Add the required libs

--- app/src/main/java/com/aggarwalankur/testhttplibs/MainActivity.java
+++ app/src/main/java/com/aggarwalankur/testhttplibs/MainActivity.java
@@ -2,12 +2,27 @@ package com.aggarwalankur.testhttplibs;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mVolleyButton, mRetrofitButton, mOkHttpButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mVolleyButton = (Button) findViewById(R.id.button_volley);
+        mRetrofitButton = (Button) findViewById(R.id.button_retrofit);
+        mOkHttpButton = (Button) findViewById(R.id.button_okhttp);
     }
 }
---
+++ app/src/main/java/com/aggarwalankur/testhttplibs/volley/VolleyActivity.java
@@ -0,0 +1,15 @@
+package com.aggarwalankur.testhttplibs.volley;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.aggarwalankur.testhttplibs.R;
+
+public class VolleyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_volley);
+    }
+}
